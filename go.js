7fec707164e304f6b38d5f03da64aa0c0296a45d
w575413642 <575413642@qq.com>
2017-08-07 17:41:49 +0800

    add webpack and css-loader,style-loader

---
+++ go.js
@@ -0,0 +1,5 @@
+function ac(){
+	return {
+		
+	}
+}
---
+++ hello.js
@@ -0,0 +1,8 @@
+require('./go.js')
+require('style-loader!css-loader!./style.css')
+
+function hello(str){
+	console.log(str)	
+}
+
+hello('123')
